8c995bcaaaafd7728168a75cd44b2c1dbad77ea8
Gleb Alektorov <gleb.alektorov@yandex.ru>
2022-12-18 18:39:33 +0500

    add lab-6

---
+++ lab-6/Laba_6_OOP/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Laba_4_1_OOP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Button_Circle = new System.Windows.Forms.Button();
+            this.Button_Square = new System.Windows.Forms.Button();
+            this.Button_Rectangular = new System.Windows.Forms.Button();
+            this.change_color_button = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.resize_box = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(950, 480);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(663, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(719, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // Button_Circle
+            // 
+            this.Button_Circle.Location = new System.Drawing.Point(29, 26);
+            this.Button_Circle.Name = "Button_Circle";
+            this.Button_Circle.Size = new System.Drawing.Size(85, 23);
+            this.Button_Circle.TabIndex = 5;
+            this.Button_Circle.Text = "Круг";
+            this.Button_Circle.UseVisualStyleBackColor = true;
+            this.Button_Circle.Click += new System.EventHandler(this.Button_Circle_Click);
+            // 
+            // Button_Square
+            // 
+            this.Button_Square.Location = new System.Drawing.Point(120, 26);
+            this.Button_Square.Name = "Button_Square";
+            this.Button_Square.Size = new System.Drawing.Size(89, 23);
+            this.Button_Square.TabIndex = 6;
+            this.Button_Square.Text = "Квадрат";
+            this.Button_Square.UseVisualStyleBackColor = true;
+            this.Button_Square.Click += new System.EventHandler(this.Button_Square_Click);
+            // 
+            // Button_Rectangular
+            // 
+            this.Button_Rectangular.Location = new System.Drawing.Point(215, 26);
+            this.Button_Rectangular.Name = "Button_Rectangular";
+            this.Button_Rectangular.Size = new System.Drawing.Size(89, 23);
+            this.Button_Rectangular.TabIndex = 7;
+            this.Button_Rectangular.Text = "Треугольник";
+            this.Button_Rectangular.UseVisualStyleBackColor = true;
+            this.Button_Rectangular.Click += new System.EventHandler(this.Button_Rectangular_Click_1);
+            // 
+            // change_color_button
+            // 
+            this.change_color_button.Location = new System.Drawing.Point(373, 12);
+            this.change_color_button.Name = "change_color_button";
+            this.change_color_button.Size = new System.Drawing.Size(95, 54);
+            this.change_color_button.TabIndex = 8;
+            this.change_color_button.Text = "Изменить цвет объекта";
+            this.change_color_button.UseVisualStyleBackColor = true;
+            this.change_color_button.Click += new System.EventHandler(this.change_color_button_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(485, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Изменить размер объекта";
+            // 
+            // resize_box
+            // 
+            this.resize_box.Location = new System.Drawing.Point(503, 46);
+            this.resize_box.Name = "resize_box";
+            this.resize_box.Size = new System.Drawing.Size(100, 20);
+            this.resize_box.TabIndex = 10;
+            this.resize_box.KeyDown += new System.Windows.Forms.KeyEventHandler(this.resize_box_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resize_box);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.change_color_button);
+            this.Controls.Add(this.Button_Rectangular);
+            this.Controls.Add(this.Button_Square);
+            this.Controls.Add(this.Button_Circle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Button_Circle;
+        private System.Windows.Forms.Button Button_Square;
+        private System.Windows.Forms.Button Button_Rectangular;
+        private System.Windows.Forms.Button change_color_button;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox resize_box;
+    }
+}
+
---
+++ lab-6/Laba_6_OOP/Form1.cs
@@ -0,0 +1,508 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Laba_6_OOP;
+using System.Runtime.CompilerServices;
+
+namespace Laba_4_1_OOP
+{
+    public partial class Form1 : Form
+    {
+        Folder folder_1 = new Folder(0);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool ctrl = false;
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == (char)Keys.ControlKey)
+                ctrl = false;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == (char)Keys.ControlKey)
+                ctrl = true;
+
+
+            if(e.KeyValue == (char)Keys.A)
+            {
+                folder_1.moveLeft(-15,0);
+                folder_1.paint(pictureBox1);
+            }
+
+            if(e.KeyValue == (char)Keys.D)
+            {
+                folder_1.moveRight(15,0);
+                folder_1.paint(pictureBox1);
+            }
+
+            if(e.KeyValue == (char)Keys.W)
+            {
+                folder_1.moveTop(0,-15);
+                folder_1.paint(pictureBox1);
+            }
+
+            if(e.KeyValue == (char)Keys.S)
+            {
+                folder_1.moveBottom(0,15);
+                folder_1.paint(pictureBox1);
+            }
+
+
+            if (e.KeyValue == (char)Keys.Delete)
+            {
+                folder_1.del_active();
+                folder_1.paint(pictureBox1);
+            }
+        }
+
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            this.ActiveControl = null;
+
+            if (ctrl != true)
+                folder_1.deact_all();
+
+            folder_1.probeg(e.X, e.Y);
+            folder_1.paint(pictureBox1);
+        }
+        private void change_color_button_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Color color = colorDialog1.Color;
+
+                folder_1.changecolor(color);
+                this.ActiveControl = null;
+                folder_1.paint(pictureBox1);
+
+            }
+        }
+
+
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            folder_1.paint(pictureBox1);
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.Text = e.X.ToString();
+            label2.Text = e.Y.ToString();
+            folder_1.picturebox1 = pictureBox1;
+        }
+
+        private void Button_Circle_Click(object sender, EventArgs e)
+        {
+            folder_1.flag_circle = true;
+            folder_1.flag_square = false;
+            folder_1.flag_triangle = false;
+        }
+        private void Button_Square_Click(object sender, EventArgs e)
+        {
+            folder_1.flag_circle = false;
+            folder_1.flag_square = true;
+            folder_1.flag_triangle = false;
+        }
+        private void Button_Rectangular_Click_1(object sender, EventArgs e)
+        {
+            folder_1.flag_circle = false;
+            folder_1.flag_square = false;
+            folder_1.flag_triangle = true;
+        }
+
+        int size;
+
+        private void resize_box_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyValue == (char)Keys.Enter)
+            {
+                size = Convert.ToInt32(resize_box.Text);
+
+                folder_1.resize(size);
+                this.ActiveControl = null;
+                folder_1.paint(pictureBox1);
+            }
+        }
+    }
+}
+
+public class Object
+{
+    protected int x;
+    protected int y;
+    protected Color color;
+    public PictureBox picturebox1;
+
+    public bool active;
+
+    public Object(int x_1, int y_1, PictureBox pictureBox)
+    {
+        active = true;
+        this.x = x_1;
+        this.y = y_1;
+        this.color = Color.Black;
+        this.picturebox1 = pictureBox;
+    }
+
+    public void change_active()
+    {
+        this.active = !(this.active);
+    }
+
+    public void activate()
+    {
+        this.active = true;
+    }
+
+    public void deactivate()
+    {
+        this.active = false;
+    }
+
+
+    public bool isActive()
+    {
+        return this.active;
+    }
+
+    public void changecolor(Color color)
+    {
+        this.color = color;
+    }
+
+    public virtual void Paint(PictureBox picturebox1, Graphics g)
+    {
+
+    }
+
+    public virtual bool Selected(int x, int y) { return false; }
+
+    public virtual void resize(int new_size) { }
+
+    public virtual void move(int x, int y) { }
+
+    protected virtual void changes_accepted(int dx,int dy,int new_size) { }
+
+
+    ~Object()
+    {
+        Console.Write("Диструктор класса object");
+    }
+}
+
+
+
+
+
+
+
+}
+
+public class Folder
+{
+    public Object[] objects;
+    public int folder_size;
+
+    public PictureBox picturebox1;
+    public bool flag_circle;
+    public bool flag_triangle;
+    public bool flag_square;
+
+    private void increase_array(Object[] objectss, int array_size, int new_array_size)
+    {
+
+        Object[] new_objects = new Object[new_array_size];
+
+        for (int i = 0; i < new_array_size; i++)
+            if (i < array_size)
+                new_objects[i] = objects[i];
+            else
+                new_objects[i] = null;
+
+        this.folder_size = new_array_size;
+        this.objects = new_objects;
+
+    }
+    public Folder(int folder_sizee)
+    {
+        Console.Write("Конструктор по умолчанию класса folder");
+        this.folder_size = folder_sizee;
+        this.objects = new Object[folder_sizee];
+        this.picturebox1 = picturebox1;
+
+        flag_circle = false;
+        flag_square = false;
+        flag_triangle = false;
+
+        for (int i = 0; i < folder_size; i++)
+        {
+            objects[i] = null;
+        }
+    }
+
+    public void deact_all()
+    {
+        for (int i = 0; i < folder_size; ++i)
+            if (objects[i] != null)
+                objects[i].deactivate();
+    }
+
+    public void del_active()
+    {
+        for (int i = 0; i < folder_size; ++i)
+            if (objects[i] != null)
+                if (objects[i].isActive())
+                    objects[i] = null;
+    }
+
+    public void probeg(int x_1, int x_2)
+    {
+        bool reactive = false;
+
+        for (int i = 0; i < folder_size; i++)
+        {
+            if (objects[i] != null)
+                if (objects[i].Selected(x_1, x_2))
+                {
+                    objects[i].change_active();
+                    reactive = true;
+                    break;
+                }
+        }
+
+        if (reactive == false)
+        {
+            if (flag_circle == true)
+            {
+                CCircle circle = new CCircle(x_1, x_2, picturebox1);
+                add_object(circle);
+
+                for (int i = 0; i < folder_size + 1; i++)
+                {
+                    if (objects[i] != null)
+                        if (objects[i].Selected(x_1, x_2))
+                        {
+                            objects[i].activate();
+                            break;
+                        }
+                }
+            }
+            else if (flag_square == true)
+            {
+                Square square = new Square(x_1, x_2, picturebox1);
+                add_object(square);
+
+                for (int i = 0; i < folder_size + 1; i++)
+                {
+                    if (objects[i] != null)
+                        if (objects[i].Selected(x_1, x_2))
+                        {
+                            objects[i].activate();
+                            break;
+                        }
+                }
+            }
+            else if (flag_triangle == true)
+            {
+                Triangle triangle = new Triangle(x_1, x_2 , picturebox1);
+                add_object(triangle);
+
+                for (int i = 0; i < folder_size + 1; i++)
+                {
+                    if (objects[i] != null)
+                        if (objects[i].Selected(x_1, x_2))
+                        {
+                            objects[i].activate();
+                            break;
+                        }
+                }
+            }
+
+        }
+
+    }
+
+    public void paint(PictureBox picturebox1)
+    {
+        Graphics g = picturebox1.CreateGraphics();
+        g.Clear(Color.White);
+
+        for (int i = 0; i < folder_size; i++)
+        {
+            if (objects[i] != null)
+                objects[i].Paint(picturebox1, g);
+        }
+    }
+
+
+
+
+    public bool check_by_index(int index)
+    {
+        if (index < folder_size)
+        {
+            if (objects[index] == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void add_object(Object something)
+    {
+        CCircle circlecheck = something as CCircle;
+
+        bool mesto = false;
+        for (int i = 0; i < folder_size; i++)
+        {
+            if (objects[i] == null)
+            {
+                mesto = true;
+                objects[i] = something;
+            }
+        }
+        if (mesto == false)
+        {
+            set_object(folder_size, something);
+        }
+
+    }
+
+    public void resize(int new_size)
+    {
+        for (int i = 0; i < folder_size; i++)
+        {
+            if (objects[i] != null)
+            {
+                if (objects[i].isActive())
+                {
+                    objects[i].resize(new_size);
+                }
+            }
+        }
+    }
+
+    public void moveLeft(int dx,int dy)
+    {
+        for(int i=0; i< folder_size; i++)
+        {
+            if (objects[i] != null)
+            {
+                if (objects[i].isActive())
+                {
+                    objects[i].move(dx,dy);
+                }
+            }
+        }
+    }
+
+    public void moveRight(int dx,int dy)
+    {
+        for (int i = 0; i < folder_size; i++)
+        {
+            if (objects[i] != null)
+            {
+                if (objects[i].isActive())
+                {
+                    objects[i].move(dx,dy);
+                }
+            }
+        }
+    }
+
+    public void moveTop(int dx, int dy)
+    {
+        for (int i = 0; i < folder_size; i++)
+        {
+            if (objects[i] != null)
+            {
+                if (objects[i].isActive())
+                {
+                    objects[i].move(dx,dy);
+                }
+            }
+        }
+    }
+
+    public void moveBottom(int dx, int dy)
+    {
+        for (int i = 0; i < folder_size; i++)
+        {
+            if (objects[i] != null)
+            {
+                if (objects[i].isActive())
+                {
+                    objects[i].move(dx,dy);
+                }
+            }
+        }
+    }
+
+    public void changecolor(Color color)
+    {
+        for(int i = 0; i < folder_size; i++)
+        {
+            if (objects[i] != null)
+            {
+                if (objects[i].isActive())
+                {
+                    objects[i].changecolor(color);
+                }
+            }
+        }
+    }
+
+    public void set_object(int index, Object something)
+    {
+        if (index >= this.folder_size)
+        {
+            increase_array(objects, folder_size, index + 1);
+            objects[index] = something;
+        }
+    }
+
+
+
+    public Object get_object(int index)
+    {
+        if (check_by_index(index) == true)
+            return this.objects[index];
+        else
+            return null;
+    }
+
+
+    ~Folder()
+    {
+        Console.Write("Хранилище было удалено");
+    }
+}
