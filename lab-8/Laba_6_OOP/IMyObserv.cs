73b16d55164fb5d5d50fa1f85beb413c04f53ef9
Gleb <gleb.alektorov@gmail.com>
2022-12-18 23:32:22 +0500

    add Interface IMyObserver

---
+++ lab-8/Laba_6_OOP/IMyObserv.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_6_OOP
+{
+    public interface IMyObservable
+    {
+        void AddObserver(IMyObserver o);
+        void RemoveObserver(IMyObserver o);
+        void NotifyCreate();
+        void NotifySelect();
+        void NotifyDelete_with_cnt();
+        void NotifyDelete_without_cnt();
+    }
+
+    public interface IMyObserver
+    {
+        void UpdateCreate_with_cnt(CShape who);
+        void UpdateSelect(CShape who);
+        void UpdateDelete_with_cnt(CShape who);
+        void UpdateDelete_without_cnt(CShape who);
+    }
+}
